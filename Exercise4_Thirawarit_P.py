22a2e044cd2d19279886684e01f7dc8d89ccf8e1
Thirawarit.p <Thirawarit.p@gmail.com>
2022-07-29 16:52:33 +0700

    Create Exercise4_Thirawarit_P.py

---
+++ Exercise4_Thirawarit_P.py
@@ -0,0 +1,11 @@
+
+Found_Eng = float(input("Input Foundation English score : "))
+Gen_Bus = float(input("Input General Business score : "))
+Intro_Com_Sys = float(input("Input Introduction of Computer Systems score : "))
+Com_Pro = float(input("Input Computer Programming score : "))
+
+print("---Your Score---")
+print("Foundation English : ", Found_Eng)
+print("General Business : ", Gen_Bus)
+print("Introduction of Computer System : ", Intro_Com_Sys)
+print("Computer Programming : ", Com_Pro)
