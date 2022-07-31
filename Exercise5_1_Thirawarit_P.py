778781bfe73aff7e0ce27d3b8209a0aa772b02e5
Thirawarit.p <Thirawarit.p@gmail.com>
2022-07-31 13:58:03 +0700

    Create Exercise5_1_Thirawarit_P.py

---
+++ Exercise5_1_Thirawarit_P.py
@@ -0,0 +1,14 @@
+import re
+
+
+if __name__ == '__main__' :
+    value1 = int(input("Input Number 1 : "))
+    value2 = int(input("Input Number 2 : "))
+    result = value1 + value2
+    print(value1, " + ", value2, " = ", result)    
+    result = value1 - value2
+    print(value1, " - ", value2, " = ", result)     
+    result = value1 * value2
+    print(value1, " * ", value2, " = ", result) 
+    result = value1 // value2
+    print(value1, " / ", value2, " = ", result) 
