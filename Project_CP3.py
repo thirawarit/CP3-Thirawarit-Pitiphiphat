1ec2726ac2b8c4b7de460ae60f94c7e0a2357809
Thirawarit.p <Thirawarit.p@gmail.com>
2022-08-11 17:07:03 +0700

    Added class ordinal

--- Project_CP3.py
+++ Project_CP3.py
@@ -1,26 +1,41 @@
 '''
-Project : ลิฟต์คอนโด 2 ห้อง
--เริ่มต้นที่ชั้น 1 ทั้ง 2 อัน
--รับค่า ห้องในชั้นนั้น โดยมีรายละเอียดคือ เลขที่ดิน/(ชั้น,เลขลำดับห้อง) เช่น 66/956
--เมื่อส่งลูกบ้านแล้ว ให้ลิฟต์ที่ใกล้ชั้นที่ลูกบ้านที่2 ไปรับลูกบ้าน ลงมา
+Project : ลิฟต์คอนโด 2 ตัว
+-เริ่มต้นที่ชั้น 1 ทั้ง 2 ตัว
+-หากลิฟต์อยู่ชั้นเดียวกันจะเรียก ลิฟต์ตัวที่ 1 เสมอ
+-หากเรียกลิฟต์ที่อยู่ต่างชั้นกัน จะเรียกลิฟต์ที่อยู่ใกล้กับชั้นนั้นที่สุด
+
+-รับค่า ชั้นที่เรียก และ ชั้นที่จะไป
+-
 '''
-class Lift_Work:
+class Setup_Lift:
+    #1st 2nd 3rd 4th 5th 6th 7th 8th 9th 10th 11st
+    ordinal_floor = []
+    num_floor = int()
+    def cal_ordinal(self):
+        for i in range(self.num_floor):
+            if 0 < (i + 1)%10 <= 3:
+                self.ordinal_floor.append(str(self.num_floor) + ['st', 'nd', 'rd'][self.num_floor%10-1])
+            else:
+                self.ordinal_floor.append(str(self.num_floor) + 'th')
+    
+
+class Lift_Work(Setup_Lift):
     call_lift = int()
     position_lift = int()
     lift_terminal = int()
-    order_floor = ['st', 'nd', 'ed', 'th', 'th', 'th', 'th', 'th']
+    ordinal_floor = Setup_Lift.ordinal_floor
     def get_passenger(self):
         print("Lift's coming...")
-        print(self.call_lift, self.order_floor[self.call_lift-1],", Lift's opening...")
+        print(self.call_lift, self.ordinal_floor[self.call_lift-1],", Lift's opening...")
     def lift_move(self):
         if self.lift_terminal < self.position_lift:
             self.position_lift = self.lift_terminal
-            print("lift's going down...", self.lift_terminal, self.order_floor[self.lift_terminal-1])
+            print("lift's going down...", self.lift_terminal, self.ordinal_floor[self.lift_terminal-1])
         elif self.lift_terminal > self.position_lift:
             self.position_lift = self.lift_terminal
-            print("lift's going up...", self.lift_terminal, self.order_floor[self.lift_terminal-1])
+            print("lift's going up...", self.lift_terminal, self.ordinal_floor[self.lift_terminal-1])
         else:
-            print("lift's opening...", self.lift_terminal, self.order_floor[self.lift_terminal-1])
+            print("lift's opening...", self.lift_terminal, self.ordinal_floor[self.lift_terminal-1])
     def check_status(self, num_lift):
         if num_lift == 1:
             print("========","\nlift1 is",self.position_lift)
@@ -28,6 +43,10 @@ class Lift_Work:
             print("lift2 is",self.position_lift,"\n"+"========")
 
 
+setup_numfloor = Setup_Lift()
+setup_numfloor.num_floor = int(input("Set lifts in the building >> :"))
+setup_numfloor.cal_ordinal()
+print(setup_numfloor.ordinal_floor)
 lift1 = Lift_Work()
 lift2 = Lift_Work()
 lift1.position_lift = 1
@@ -56,3 +75,4 @@ while True:
         lift2.lift_move()    
     lift1.check_status(1)
     lift2.check_status(2)
+
