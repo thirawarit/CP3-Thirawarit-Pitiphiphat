1d5f49853426d83230cd9c07e72a34ce5de971ad
Thirawarit.p <Thirawarit.p@gmail.com>
2022-10-31 16:27:15 +0700

    Update Project_LiftInTPCondo_Thirawarit_P.py
    new version for present

--- Project_LiftInTPCondo_Thirawarit_P.py
+++ Project_LiftInTPCondo_Thirawarit_P.py
@@ -14,11 +14,16 @@ input =
 4.integer: รับเลขชั้นที่ต้องการจะไป
 **** หากต้องการหยุดloop ให้ใส่อักษร alphabet ลงใน input
 '''
+import time
+
+
 class Setup_Lift:
     #1st 2nd 3rd 4th 5th 6th 7th 8th 9th 10th 11st
     ordinal_floor = []
-    num_floor = int()
-    def cal_ordinal(self):
+    def __init__(self) -> None:
+        pass
+    def cal_ordinal(self, num_floor: int):
+        self.num_floor = num_floor
         for i in range(self.num_floor):
             if 0 < (i+1)%10 <= 3:
                 self.ordinal_floor.append(str(i+1) + ['st', 'nd', 'rd'][i%10])
@@ -28,22 +33,31 @@ class Setup_Lift:
 
 class Lift_Work(Setup_Lift):
     call_lift = int()
-    position_lift = int()
+    position_lift = 1
     lift_terminal = int()
     ordinal_floor = Setup_Lift.ordinal_floor
-    def get_passenger(self):
+    def get_passenger(self, call_lift: int):
+        self.call_lift = call_lift
         print("Lift's coming...")
+        time.sleep(2)
         print(self.ordinal_floor[self.call_lift-1],", Lift's opening...")
         self.position_lift = self.call_lift
-    def lift_move(self):
+    def lift_move(self, lift_terminal: int):
+        self.lift_terminal = lift_terminal
         if self.lift_terminal < self.position_lift:
             self.position_lift = self.lift_terminal
-            print("Lift's going down...", self.ordinal_floor[self.lift_terminal-1])
+            print("Lift's going down...")
+            time.sleep(2)
+            print(self.ordinal_floor[self.lift_terminal-1])
         elif self.lift_terminal > self.position_lift:
             self.position_lift = self.lift_terminal
-            print("Lift's going up...", self.ordinal_floor[self.lift_terminal-1])
+            print("Lift's going up...")
+            time.sleep(2)
+            print( self.ordinal_floor[self.lift_terminal-1])
         else:
-            print("lift's opening...", self.ordinal_floor[self.lift_terminal-1])
+            print("lift's opening...")
+            time.sleep(0.5)
+            print(self.ordinal_floor[self.lift_terminal-1])
     def check_status(self, num_lift):
         if num_lift == 1:
             print("========","\nLift1 is",self.position_lift)
@@ -51,36 +65,37 @@ class Lift_Work(Setup_Lift):
             print("Lift2 is",self.position_lift,"\n"+"========")
 
 
+code_service = 505
 setup_numfloor = Setup_Lift()
-setup_numfloor.num_floor = int(input("Set lifts in the building >> :"))
-setup_numfloor.cal_ordinal()
+setup_numfloor.cal_ordinal(int(input("Set lifts in the building >> :")))
 print(setup_numfloor.ordinal_floor)
 lift1 = Lift_Work()
 lift2 = Lift_Work()
-lift1.position_lift = 1
-lift2.position_lift = 1
-lift1_go = input("lift1's opening... , Select Floor that you want : ")
-if lift1_go.isalpha() or int(lift1_go) > setup_numfloor.num_floor:
-    print("************\nERROR System input wrong data!!!\n************")
-else:
-    lift1.position_lift = int(lift1_go)
+
+lift1.check_status(1)
+lift2.check_status(2)
+while True:
+    try:
+        call = int(input('call lift from floor : '))
+    except ValueError:
+        print("************", 
+              "ERROR System input wrong data!!!", 
+              "Only interger such as : 0, 12, 345", 
+              "************", 
+              sep='\n'
+              )
+        break
+    if call == code_service:
+        print("Manual mode - SERVICE")
+        break
+    elif abs(lift1.position_lift-call) <= abs(lift2.position_lift-call):
+        lift1.get_passenger(call)
+        lift_terminal = int(input('Select Floor that you want : '))
+        lift1.lift_move(lift_terminal)
+    elif abs(lift1.position_lift-call) > abs(lift2.position_lift-call):
+        lift2.get_passenger(call)
+        lift_terminal = int(input('Select Floor that you want : '))
+        lift2.lift_move(lift_terminal)
     lift1.check_status(1)
     lift2.check_status(2)
-    while lift1_go.upper != 'STOP':
-        personcalls = input('call lift from floor : ')
-        if personcalls.isalpha() or int(personcalls) > setup_numfloor.num_floor:
-            print("************\nEmergency Stop!\n************")
-            break
-        elif abs(lift1.position_lift-int(personcalls)) <= abs(lift2.position_lift-int(personcalls)):
-            lift1.call_lift = int(personcalls)
-            lift1.get_passenger()
-            lift1.lift_terminal = int(input('Select Floor that you want : '))
-            lift1.lift_move()
-        elif abs(lift1.position_lift-int(personcalls)) > abs(lift2.position_lift-int(personcalls)):
-            lift2.call_lift = int(personcalls)
-            lift2.get_passenger()
-            lift2.lift_terminal = int(input('Select Floor that you want : '))
-            lift2.lift_move()
-        lift1.check_status(1)
-        lift2.check_status(2)
 
